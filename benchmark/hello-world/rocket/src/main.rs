4f3c9e74006251c95d483bcec9db442fbad8a698
paulip1792 <paulip1792@outlook.com>
2025-03-10 22:51:11 +0800

    chore(benchmark): Use release config for rocket
    Use release config for rocket
    
    Signed-off-by: paulip1792 <paulip1792@outlook.com>

--- benchmark/hello-world/rocket/src/main.rs
+++ benchmark/hello-world/rocket/src/main.rs
@@ -1,4 +1,4 @@
-use rocket::{get, launch, routes, Config};
+use rocket::{config::LogLevel, get, launch, routes, Config};
 
 #[get("/")]
 fn hello() -> &'static str {
@@ -10,6 +10,8 @@ fn rocket() -> _ {
     let config = Config {
         port: 3000,
         address: "127.0.0.1".parse().unwrap(),
+        profile: Config::RELEASE_PROFILE,
+        log_level: LogLevel::Critical,
         ..Config::debug_default()
     };
     rocket::custom(&config).mount("/", routes![hello])
